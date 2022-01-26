490efd6fa63fb03b2201eb2a5cc7fa7a7109900c
Jorge Luis Rivera Ladino <ext_jorgrive@mercadolibre.com.co>
2022-01-25 22:10:53 -0500

    Initial Commit

---
+++ APOD-iOS-CLEAN/APOD_iOS_CLEANApp.swift
@@ -0,0 +1,17 @@
+//
+//  APOD_iOS_CLEANApp.swift
+//  APOD-iOS-CLEAN
+//
+//  Created by Jorge Luis Rivera Ladino on 25/01/22.
+//
+
+import SwiftUI
+
+@main
+struct APOD_iOS_CLEANApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ APOD-iOS-CLEAN/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  APOD-iOS-CLEAN
+//
+//  Created by Jorge Luis Rivera Ladino on 25/01/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
